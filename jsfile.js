8790eaef05d2832afe2eca6e30ac38db1545c64e
shrewdas <sonjw0116@naver.com>
2024-11-18 14:59:25 +0900

    Create jsfile.js

---
+++ jsfile.js
@@ -0,0 +1,15 @@
+    function compareNumbers() {
+        let a = document.getElementById('inputA').value;
+        let a = document.getElementById('inputB').value;
+
+        a = Number(a);
+        b = Number(b);
+
+        if (a > b) {
+            document.getElementById('result').innerText = "A가 B보다 큼";
+        } else if (a < b) {
+            document.getElementById('result').innerText = "A가 B보다 작음";
+        } else {
+            document.getElementById('result').innerText = "A가 B가 같음";
+        }
+    }
